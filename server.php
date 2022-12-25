c4cc2d27e8366919f91db6866b23682e999608bd
Daniil <daniil_sharukha@icloud.com>
2022-12-25 14:05:04 +0600

    модульная структура + webpack

--- server.php
+++ server.php
@@ -1,3 +1,3 @@
 <?php
-$_POST = json_decode(file_get_contents('php://input'), true); //на php коде получаем JSON данные, с которыми можно работать
+$_POST = json_decode( file_get_contents("php://input"), true );
 echo var_dump($_POST);
